9964ad349db18729d5bb3edbd38b006744d140ad
Maks6831 <ma.khan6971@gmail.com>
2023-03-21 21:38:18 +0000

    added basic page structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Footer from './components/Footer';
+
+
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/products' element={<Products />} />
+
+        </Routes>
+        <Footer/>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Products.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Products = () => {
+    return (
+        <div>This is the products page</div>
+    )
+}
+
+export default Products;
