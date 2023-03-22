746df7ad54c2c6d3b50541cc139b7b7eed5106ee
Benjamin <bcott93@gmail.com>
2023-03-22 22:55:02 +0000

    merge conflicts

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { useAuth, AuthProvider } from "./contexts/AuthContext";
+import { auth, provider } from "./pages/Firebase";
+import { AppStateProvider, useAppState }from "./providers/app-state"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <AuthProvider>
     <AppStateProvider>
-      <AuthProvider>
       <App />
     </AppStateProvider>
     </AuthProvider>
