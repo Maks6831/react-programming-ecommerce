1876ff807cd8ca75d5d8d765a80581d5d913347a
Maks6831 <ma.khan6971@gmail.com>
2023-03-22 21:59:42 +0000

    created about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,7 @@
 import React from "react";
 import '../styles/About.css';
+import '../styles/Home.css';
+import { Link } from "react-router-dom";
 
 const About = () => {
     return (
@@ -10,7 +12,31 @@ const About = () => {
                     <p className="flex-child">Feel free to browse our products</p>
                 </div>
             </div>
-            <div className="hero"></div>
+            <div className="buttonContainer">
+        <Link to='/products' className="productButton">Discover our Products</Link>
+        </div>
+            
+            <div className="kitSection">
+        <h1>Checkout our kits</h1>
+        <div className="optionsDiv">
+            <div className="card">
+            <div className=" kitCard newbie">
+            </div>
+            <div className="kitTitle">Newbie Kit</div>
+            </div>
+            <div className="card">
+            <div className=" kitCard ninja">
+            </div>
+            <div className="kitTitle">Ninja Kit</div>
+            </div>
+            <div className="card">
+            <div className=" kitCard master">
+            </div>
+            <div className="kitTitle">Master Kit</div>
+            </div>
+        </div>
+        </div>
+            
         </div>
     )
 }
