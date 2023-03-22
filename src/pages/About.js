f5b1af99b73b81565b688bc492c19618feb60cc3
Maks6831 <ma.khan6971@gmail.com>
2023-03-22 21:10:59 +0000

    started about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,17 @@
 import React from "react";
+import '../styles/About.css';
 
 const About = () => {
     return (
-        <div>This is the about page</div>
+        <div>
+            <div className="hero">
+                <div className="hero-text">
+                    <h1 className="flex-child">Meet all of your coding needs</h1>
+                    <p className="flex-child">Feel free to browse our products</p>
+                </div>
+            </div>
+            <div className="hero"></div>
+        </div>
     )
 }
 
