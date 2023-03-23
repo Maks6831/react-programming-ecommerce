3ce5571104430ae8ae3dd248793d7ef997a3f9c6
Benjamin <bcott93@gmail.com>
2023-03-23 20:14:59 +0000

    removed Modal component, added code into products. Added basic local storage to Products

--- src/components/LocalStorage.js
+++ src/components/LocalStorage.js
@@ -1,16 +1,3 @@
-// Products page local storage
-
-import React, { useState } from 'react'
-
-const [productID, setProductID] = useState({})
-
-const saveClick = () => {
-    localStorage.setItem('productID', JSON.stringify(productID))
-    console.log('productID: ' + productID)
-}
-
-
-<button onclick={handleSaveClick}> Add to Basket </button>
 
 // Basket Page 
 
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useState } from 'react'
 import '../styles/Product.css'
 
+
+
+
 const Product = (props) => {
+
+    const [showModal, setShowModal] = useState(false);
+    const [productID, setProductID] = useState({})
+
+    function hideModal() {
+      setShowModal(false);
+    }
+  
+    function addToCart() {
+        localStorage.setItem('productID', JSON.stringify(productID))
+        console.log('productID: ' + productID)
+        setShowModal(true);
+    }
+
+    function goToBasket() {
+        window.location.replace('../pages/Basket')
+      }
+
     return (
         <div className="Card">
             <img src={props.image} alt='productPicture' className="product-image" />
             <p className="p-tag">{props.name}</p>
             <p className="p-tag">price: £{props.price}</p>
-            <button className="product-button"> Add to cart</button>
+            <button className="product-button" onClick={addToCart}> Add to cart</button>
+            {showModal && (
+                <div className="modal-content">
+                <button className="button-navigation" onClick={hideModal}>Continue shopping</button>
+                <button className="button-navigation" onClick={goToBasket}>Go to basket</button>
+                </div>
+            )}
         </div>
 
     )
