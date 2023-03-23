ff4e6682664e2ea2bf6ee69efe3b5e5c227854d0
Senseilein <73607360+senseilein@users.noreply.github.com>
2023-03-23 14:21:00 +0000

    Implement handleChange() function to track form inputs

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -8,6 +8,21 @@ export default function ContactForm() {
   const { formData, setFormData } = useAppState();
   console.log(formData);
 
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    console.log(event.target.value);
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(formData);
+  };
+
   return (
     <>
       <div className="form-container">
@@ -15,35 +30,42 @@ export default function ContactForm() {
           <span>[</span> Contact us <span>]</span>
         </h2>
         <hr />
-        <form className="form">
+
+        <form className="form" onSubmit={handleSubmit}>
           <div className="form-input-group">
-            <label htmLfor="name">Name:</label>
+            <label htmlFor="name">Name:</label>
             <input
+              name="name"
               type="text"
               placeholder="Enter your name"
               className="form-input"
               id="email"
+              onChange={handleChange}
             />
           </div>
 
           <div className="form-input-group">
-            <label htmLfor="email" id="email-label">
+            <label htmlFor="email" id="email-label">
               Email:
             </label>
             <input
+              name="email"
               type="email"
               placeholder="Enter your email address"
               className="form-input"
               id="email"
+              onChange={handleChange}
             />
           </div>
           <div className="form-textarea-group">
-            <label htmLfor="textarea">Message:</label>
+            <label htmlFor="textarea">Message:</label>
             <textarea
+              name="message"
               placeholder="Write your message"
               className="form-input"
               rows="5"
               id="contact-textarea"
+              onChange={handleChange}
             />
           </div>
 
