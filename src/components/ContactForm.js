884983a509841b027236fcd755e179f39a367964
Senseilein <73607360+senseilein@users.noreply.github.com>
2023-03-23 13:16:22 +0000

    Create contact-form and style it

---
+++ src/components/ContactForm.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+import "../styles/ContactForm.css";
+
+export default function ContactForm() {
+  return (
+    <>
+      <div className="form-container">
+        <h2>
+          <span>[</span> Contact us <span>]</span>
+        </h2>
+        <hr />
+        <form className="form">
+          <div className="form-input-group">
+            <label for="name">Name:</label>
+            <input
+              type="text"
+              placeholder="Enter your name"
+              className="form-input"
+              id="email"
+            />
+          </div>
+
+          <div className="form-input-group">
+            <label for="email" id="email-label">
+              Email:
+            </label>
+            <input
+              type="email"
+              placeholder="Enter your email address"
+              className="form-input"
+              id="email"
+            />
+          </div>
+          <div className="form-textarea-group">
+            <label for="textarea">Message:</label>
+            <textarea
+              placeholder="Write your message"
+              className="form-input"
+              rows="5"
+              id="contact-textarea"
+            />
+          </div>
+
+          <button id="submit-btn">Send</button>
+        </form>
+      </div>
+    </>
+  );
+}
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,13 @@
 import React from "react";
+import ContactForm from "../components/ContactForm";
 
 const Contact = () => {
-    return (
-        <div>This is the contact page</div>
-    )
-}
+  return (
+    <>
+      <div>This is the contact page</div>
+      <ContactForm />
+    </>
+  );
+};
 
 export default Contact;
