887c2ec00fcc8657462c5228eb197cd2d5a99bf3
Senseilein <73607360+senseilein@users.noreply.github.com>
2023-03-23 16:46:29 +0000

    Add top section of Contact Page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,21 @@
 import React from "react";
 import ContactForm from "../components/ContactForm";
 // import "../styles/About.css";
-// import "../styles/Home.css";
+import "../styles/Home.css";
+import "../styles/Contact.css";
 
 const Contact = () => {
   return (
     <>
-      <div>This is the contact page</div>
+      <section className="Hero">
+        <div className="heroOpacity">
+          <h1 id="contact-page-h1">
+            We deliver quality tech
+            <br />[ <span>byte by byte</span> ]
+          </h1>
+        </div>
+      </section>
+
       <ContactForm />
     </>
   );
