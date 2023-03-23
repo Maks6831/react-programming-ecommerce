b1aa1eaec85e19ed69c10cfa9b60af6a2d5507b4
Senseilein <73607360+senseilein@users.noreply.github.com>
2023-03-23 22:19:38 +0000

    Setup basic structure for item section in Basket page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './styles/App.css';
-import Header from './components/Header';
-import Home from './pages/Home';
-import Products from './pages/Products';
-import Footer from './components/Footer';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Basket from './pages/Basket';
-import NavbarSignup from './components/NavBarSignUp'
-
-
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./styles/App.css";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Footer from "./components/Footer";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Basket from "./pages/Basket";
+import NavbarSignup from "./components/NavBarSignUp";
 
 function App() {
   return (
@@ -18,14 +15,14 @@ function App() {
       <div>
         <Header />
         <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/about' element={<About/>}/>
-          <Route path='/products' element={<Products />} />
-          <Route path='/contact' element={<Contact />}/>
-          <Route path='/basket' element={<Basket/>}/>
-          <Route path='/login' element={<NavbarSignup/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/basket" element={<Basket />} />
+          <Route path="/login" element={<NavbarSignup />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </div>
     </Router>
   );
--- src/pages/Basket.js
+++ src/pages/Basket.js
@@ -1,10 +1,42 @@
 import React from "react";
 
+import "../styles/Basket.css";
 
 const Basket = () => {
-    return (
-        <div>this is the basket page</div>
-    )
-}
+  return (
+    <div id="test-div">
+      <section id="basket-container">
+        <h1 id="basket-h1">
+          <span>[</span> Your Basket <span>]</span>
+        </h1>
+        <section className="flex-col">
+          <ul className=" flex-col item-list" id="basket">
+            <li className="flex-row item-header">
+              <li className="head name-col">Items</li>
+              <li className="head quantity-col">Quantity</li>
+              <li className="head price-col">Price</li>
+              <li className="head currency-col"> </li>
+            </li>
+            <li className=" flex-row item">
+              <li className=" basket-card name">
+                <a href="/">Dummy product</a>
+              </li>
+              <li className=" basket-card quantity">5</li>
+              <li className=" basket-card price">120</li>
+              <li className=" basket-card currency">GBP</li>
+            </li>
+            <li className=" flex-row item">
+              <li className=" basket-card name">Another one</li>
+              <li className=" basket-card quantity">1</li>
+              <li className=" basket-card price">50</li>
+              <li className=" basket-card currency">GBP</li>
+            </li>
+          </ul>
+        </section>
+        <section id="checkout"></section>
+      </section>
+    </div>
+  );
+};
 
 export default Basket;
