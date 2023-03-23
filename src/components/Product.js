28473b1fd58ed6ed54e0c3731fc77e6c5d1d70aa
Benjamin <bcott93@gmail.com>
2023-03-23 21:52:46 +0000

    Conditional rendering of modal

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/Product.css'
 import itemInfo from "../itemInfo.json";
+import { useAppState } from '../providers/app-state'
 
 
 
@@ -8,7 +9,18 @@ import itemInfo from "../itemInfo.json";
 const Product = (props) => {
 
     const [showModal, setShowModal] = useState(false);
-   
+    const {formData, state, setState} = useAppState()
+
+    // useEffect(() => {
+    //     setState(props.id)
+    // }, [])
+
+    useEffect(() => {
+        if (state) {
+            console.log(state)
+        }
+    }, [state])
+    
 
     function hideModal() {
       setShowModal(false);
@@ -16,6 +28,8 @@ const Product = (props) => {
   
     function addToCart() {
         console.log(props.name)
+        // Set the global state to the ID of the product
+        setState(props.id)
         // Get the ID of the current product from its props
         const findProductID = props.id
         // Gets the saved products from local storage, or creates an empty array if none are found
@@ -40,16 +54,16 @@ const Product = (props) => {
             <p className="p-tag">{props.name}</p>
             <p className="p-tag">price: £{props.price}</p>
             <button className="product-button" onClick={addToCart}> Add to cart</button>
-            {showModal && (
+            {showModal && state === props?.id ? (
                 <div className="modal">
                 <div className="modal-content">
-                    <h5 className="modal-title"> Your item has been added to the cart!</h5>
+                    <h5 className="modal-title"> {props.name} has been added to the cart!</h5>
                     <p> Continue shopping or go to checkout!</p>
                 <button className="button-navigation" onClick={hideModal}>Continue shopping</button>
                 <button className="button-navigation" onClick={goToBasket}>Go to basket</button>
                 </div>
                 </div>
-            )}
+            ):<></>} 
         </div>
 
     )
