c4e338c6095fd928b45d24eee39b3b9958b7ca9c
Maks6831 <ma.khan6971@gmail.com>
2023-03-23 22:55:26 +0000

    added level card components to items

--- src/pages/Item.js
+++ src/pages/Item.js
@@ -4,13 +4,19 @@ import { useState } from "react";
 import { useParams } from "react-router";
 import itemInfo from "../itemInfo.json"
 import Productrenderer from "../components/Productrenderer";
-
+import Product from "../components/Product";
 const Item = () => {
     const [currentItem, setCurrentItem] = useState({})
+    const [relatedItems, setRelatedItems] = useState({});
     const { item } = useParams()
     useEffect(()=>{
-        setCurrentItem(itemInfo.filter((id)=>((id.name === item)))) 
+        setCurrentItem(itemInfo.filter((id)=>((id.name === item))))
+        setRelatedItems(itemInfo.filter((obj) => (currentItem[0]?.level === obj.level && currentItem[0].name !== obj.name)))  
     },[])
+    
+    
+
+    
 
     
     
@@ -27,6 +33,20 @@ const Item = () => {
 
 
             />
+
+            <h1>Other {currentItem[0].level} items to this item</h1>
+            {relatedItems.map((item)=>(<Product
+            id={item.id}
+            key={item.id}
+            name={item.name}
+            image={item.image}
+            description={item.description}
+            price={item.price}
+            
+            />))
+            }
+            
+
         </div>
     )
 }
