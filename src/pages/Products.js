787bb4b65c9b777a7c39a8c139d43adf15747af5
Maks6831 <ma.khan6971@gmail.com>
2023-03-24 20:21:44 +0000

    added buttons

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Product from "../components/Product";
 import itemInfo from "../itemInfo.json";
 
 const Products = () => {
+    const [productArray, setProductArray] = useState(itemInfo);
+    const [open, setOpen] = useState(false);
+    const handleButton = (e) => {
+        setProductArray(itemInfo.filter((item)=>(item.level === e.target.value || e.target.value === item.type)))
+    }
+
     return (
         <div>
-            {itemInfo.map((item)=>(
+            <div className="filter-section">
+                <div onClick={()=> setOpen(!open)}>Filter</div>
+                {open && <div>Hello</div>}
+                <button value="keyboard" onClick={handleButton}>Keyboards</button>
+<button value="mouse" onClick={handleButton}>Mice</button>
+<button value="monitor" onClick={handleButton}>Monitors</button>
+                <button value="chair" onClick={handleButton}>Desk Chairs</button>
+
+                <button value="Ninja" onClick={handleButton}>Ninja</button>
+                <button value="Newbie"onClick={handleButton}>Newbie</button>
+                <button value="Master"onClick={handleButton}>Merge Master</button>
+            </div>
+
+            {productArray.map((item)=>(
             <Product
             key={item.id}
             id={item.id}
