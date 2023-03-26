3637a7347769e3c71627cc72ef64fcab44d8e18e
Senseilein <73607360+senseilein@users.noreply.github.com>
2023-03-26 11:43:11 +0100

    Change id of wrapper div in Basket

--- src/pages/Basket.js
+++ src/pages/Basket.js
@@ -1,85 +1,69 @@
-import React, { useState, useEffect } from "react"
-import { Link } from "react-router-dom"
-import BasketItem from "../components/BasketItem"
-import "../styles/Basket.css"
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import BasketItem from "../components/BasketItem";
+import "../styles/Basket.css";
 
 export default function Basket() {
-
-  const [ localData, setLocalData] = useState([])
-  const [ savedProducts, setSavedProducts] = useState([])
+  const [localData, setLocalData] = useState([]);
+  const [savedProducts, setSavedProducts] = useState([]);
 
   useEffect(() => {
- 
-    setLocalData(JSON.parse(localStorage.getItem("savedProducts")) || [])
-
-  }, [])
+    setLocalData(JSON.parse(localStorage.getItem("savedProducts")) || []);
+  }, []);
 
   useEffect(() => {
     if (localData.length > 0) {
-      let localProducts = []
-      console.log("saved products: " + localData)
+      let localProducts = [];
+      console.log("saved products: " + localData);
       for (let i = 0; i > localData.length; i++) {
-      localProducts.push({ 
-        id: localData[i].id, 
-        name: localData[i].name, 
-        price: localData[i].price, 
-        image: localData[i].image })
-        
-      console.log("test ", localProducts)
+        localProducts.push({
+          id: localData[i].id,
+          name: localData[i].name,
+          price: localData[i].price,
+          image: localData[i].image,
+        });
+
+        console.log("test ", localProducts);
       }
     }
-     
-}, [localData])
-    
-      
-   
+  }, [localData]);
 
   return (
-
-    <div id="test-div">
-    <section id="basket-container">
-      <h1 id="basket-h1">
-        <span>[</span> Your Basket <span>]</span>
-      </h1>
-      <section className="flex-col">
-        <ul className=" flex-col item-list" id="basket">
-          <ul className="flex-row item-header">
-            <li className="head name-col">Items</li>
-            <li className="head quantity-col">Quantity</li>
-            <li className="head price-col">Price</li>
-            <li className="head currency-col"> </li>
-          </ul>
+    <div id="wrapper">
+      <section id="basket-container">
+        <h1 id="basket-h1">
+          <span>[</span> Your Basket <span>]</span>
+        </h1>
+        <section className="flex-col">
+          <ul className=" flex-col item-list" id="basket">
+            <ul className="flex-row item-header">
+              <li className="head name-col">Items</li>
+              <li className="head quantity-col">Quantity</li>
+              <li className="head price-col">Price</li>
+              <li className="head currency-col"> </li>
+            </ul>
             <ul className="basket">
-          {localData?.map(product => (
-          
-            <BasketItem 
-              key={product.id}
-              productID={product.id}
-              name={product.name}
-              
-              price={product.price}
-              />
-            ))}
+              {localData?.map((product) => (
+                <BasketItem
+                  key={product.id}
+                  productID={product.id}
+                  name={product.name}
+                  price={product.price}
+                />
+              ))}
+            </ul>
           </ul>
-           
-         </ul>
-       </section>
-       <section className="flex-col" id="checkout">
-     <h2>
-           Total: <span>500</span> GBP
-         </h2>
-     <button className="checkout-btn">Checkout</button>
-      <Link to="/products" className="purple-link">
-     Continue Shopping
-   </Link>
-  </section>
- </section>
-</div>
-  
-
-  )
+        </section>
+        <section className="flex-col" id="checkout">
+          <h2>
+            Total: <span>500</span> GBP
+          </h2>
+          <button className="checkout-btn">Checkout</button>
+          <Link to="/products" className="purple-link">
+            Continue Shopping
+          </Link>
+        </section>
+      </section>
+    </div>
+  );
 }
-
-
-
-
