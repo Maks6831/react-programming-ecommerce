ea43022b847cbd80f7d2a06ce65204f2ab9cfacb
Maks6831 <ma.khan6971@gmail.com>
2023-03-26 19:01:18 +0100

    product renderer now has nice css and animations

--- src/components/Productrenderer.js
+++ src/components/Productrenderer.js
@@ -4,10 +4,10 @@ import '../styles/Productrenderer.css'
 const Productrenderer = (props) => {
     return (
         <div>
-            <div className="item-section">
-            <h1 className="item-h1-tag">{props.name}</h1>
+            <div className="item-section fade-in">
+            <h1 className="item-h1-tag fade-in-2">{props.name}</h1>
         
-        <div className="info">
+        <div className="info fade-in-3">
         
         <img src={props.image} className="item-image" alt='itemImage'/> 
         
