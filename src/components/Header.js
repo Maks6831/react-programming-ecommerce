16aa20af52cb71fd6d6cbf5443751032f2372ab8
Maks6831 <ma.khan6971@gmail.com>
2023-03-27 15:44:42 +0100

    Added code

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,10 @@ import React from "react";
 import Navbar from "./Navbar";
  
 const Header = () => {
+    const screenWidthOne = window.matchMedia("(max-width: 854px)");
+    console.log(window.innerWidth);
+
+
     return (
         <header><Navbar /></header>
     )
