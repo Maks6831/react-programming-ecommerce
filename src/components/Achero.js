4f95f813e705ae2d6da356be0b805987fc873e16
Maks6831 <ma.khan6971@gmail.com>
2023-03-28 19:52:29 +0100

    added animations to homepage

--- src/components/Achero.js
+++ src/components/Achero.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import 'aos/dist/aos.css';
+import AOS from 'aos';
 
 const Achero = () => {
+  useEffect(()=>{
+    AOS.init({duration:2000});
+  },[])
   return (
     <div className="hero">
-      <div className="hero-text">
-        <h1 className="flex-child">Meet all of your coding needs</h1>
+      <div className="hero-text" data-aos="fade-right">
+        <h1 className="flex-child" >Meet all of your coding needs</h1>
         <p className="flex-child hero-paragraph">
           Welcome to ByteBazaar, your one-stop shop for all your coding hardware
           needs.
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../styles/Home.css";
 import "../styles/oldHome.css";
 import { Link } from "react-router-dom";
 import Achero from "../components/Achero";
+import 'aos/dist/aos.css';
+import AOS from 'aos';
 
 const Home = () => {
+  useEffect(()=>{
+    AOS.init({duration:2000});
+
+
+  },[])
   return (
     <div>
       <Achero />
       
       <div className="buttonContainer">
-        <Link to="/products" className="productButton">
+        <Link to="/products" className="productButton" data-aos="fade-right">
           Discover our Products
         </Link>
       </div>
       <section className="about">
       <div className="about-section">
-        <div className="about-title">
+        <div className="about-title" data-aos="fade-right">
         <h2>What we are about</h2>
         </div>
        
         
-        <p className="about-description">
+        <p className="about-description" data-aos="fade-left">
           At ByteBazaar, we offer a wide range of high-quality equipment and
           accessories from top brands for your coding needs. Our e-commerce
           platform provides a secure and convenient shopping experience, and our
