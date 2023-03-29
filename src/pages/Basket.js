8d97e9ce8d931abe94c79e36a2860e6caf8cfd3f
Maks6831 <ma.khan6971@gmail.com>
2023-03-30 00:37:15 +0100

    basket prices taking into account quantity, total: now working

--- src/pages/Basket.js
+++ src/pages/Basket.js
@@ -27,10 +27,11 @@ export default function Basket() {
     if (localData.length > 0) {
       let localProducts = [];
       for (let i = 0; i < localData.length; i++) {
+        localData[i].total = localData[i].price * localData[i].quantity
         localProducts.push({
           id: localData[i].id,
           name: localData[i].name,
-          price: localData[i].price,
+          price: localData[i].total,
           image: localData[i].image,
         });
         window.scrollTo(0, 0);
